1786a7392785ae69ae54484dfe5d7724b50467e7
masattv <k3absorb430@gmail.com>
2025-04-05 08:42:50 +0900

    Initial commit

---
+++ src/Todo.jsx
@@ -0,0 +1,35 @@
+import './styles.css';
+
+export const App = () => {
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>TODOでした</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
